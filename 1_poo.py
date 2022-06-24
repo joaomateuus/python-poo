6d36b0f3af66b52bad334c5c77b3a8738671247f
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-24 14:28:14 -0300

    commiting commit

--- 1_poo.py
+++ 1_poo.py
@@ -1,36 +1,35 @@
 class Animal():
-    def __init__(self, nome, patas, alimento, ecosistema):
+    def __init__(self, nome, alimento, ecosistema):
         self.nome = nome
-        self.patas = patas
         self.alimento = alimento
         self.ecosistema = ecosistema
     
     def __str__(self) -> str:
-        return f'{self.nome} {self.patas} {self.alimento} {self.ecosistema}'
+        return f'{self.nome} {self.alimento} {self.ecosistema}'
 
 
 class Pet(Animal):
-    def __init__(self, nome, raça, patas, alimento, ecosistema, idade, tipo):
-        super().__init__(nome, patas, alimento, ecosistema)
+    def __init__(self, nome, raça, alimento, ecosistema, idade, tipo):
+        super().__init__(nome, alimento, ecosistema)
         self.idade = idade
         self.raça = raça
         self.tipo = tipo
     
     def __str__(self) -> str:
-        return f'Ola eu sou um {self.tipo} meu nome é {self.nome} eu tenho {self.patas} patas gosto muito de {self.alimento} e tenho {self.idade}'
+        return f'Ola eu sou um {self.tipo} meu nome é {self.nome} gosto muito de {self.alimento} e tenho {self.idade}'
 
-gato = Pet(tipo='gato', nome='Linux', raça='branco', patas=4, alimento='ração', idade='6 meses', ecosistema='Terrestre')
-cachorro = Pet(tipo='cachorro', nome='Lola', raça='yorkshire', patas=4, alimento='ração', idade='um ano e meio', ecosistema='Terrestre')
+gato = Pet(tipo='gato', nome='Linux', raça='branco', alimento='ração', idade='6 meses', ecosistema='Terrestre')
+cachorro = Pet(tipo='cachorro', nome='Lola', raça='yorkshire', alimento='ração', idade='um ano e meio', ecosistema='Terrestre')
 
 print(gato)
 print(cachorro)
 
 class Invertebrados(Animal):
-    def __init__(self, nome, alimento, ecosistema, patas, coluna):
-        super().__init__(nome, alimento, ecosistema, patas)
+    def __init__(self, nome, alimento, ecosistema, coluna):
+        super().__init__(nome, alimento, ecosistema)
         self.coluna = coluna
     def __str__(self):
-        return f'{self.nome} {self.patas} {self.alimento} {self.ecosistema} {self.coluna}'
-minhoca = Invertebrados(nome='minhoco', patas=0,  alimento='residuos', ecosistema='terrestre', coluna='nao possui')
+        return f'{self.nome} {self.alimento} {self.ecosistema} {self.coluna}'
+minhoca = Invertebrados(nome='minhoco', alimento='residuos', ecosistema='terrestre', coluna='nao possui')
 
 print(minhoca)
